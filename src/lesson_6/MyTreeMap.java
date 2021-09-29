d1d58cbba75a69851ceca6205931e0703b09c893
Lenovo-PC <Ulya2811@yandex.ru>
2021-09-30 01:08:12 +0300

    lesson_6

---
+++ src/lesson_6/MyTreeMap.java
@@ -0,0 +1,226 @@
+package lesson_6;
+
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+public class MyTreeMap<K extends Comparable<K>,V extends Comparable<V>> {
+    private Node root;
+
+    private class Node {
+        K key;
+        V value;
+        Node left;
+        Node right;
+        int size;
+        int height;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.size = 1;
+            this.height = 1;
+        }
+    }
+
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.size;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    private void checkKeyNotNull(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key == null");
+        }
+    }
+
+    public boolean contains(K key) {
+        return get(key) != null;
+    }
+
+    public V get(K key) {
+        checkKeyNotNull(key);
+        return get(root, key);
+    }
+
+    private V get(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp == 0) {
+            return node.value;
+        } else if (cmp < 0) {
+            return get(node.left, key);
+        } else {
+            return get(node.right, key);
+        }
+    }
+
+    public void put(K key, V value) {
+        checkKeyNotNull(key);
+        if (value == null) {
+            // remove(key)
+            return;
+        }
+        root = put(root, key, value);
+    }
+
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
+            return new Node(key, value);
+        }
+        int cmp = value.compareTo(node.value);
+        if (cmp == 0) {
+            node.value = value;
+        } else if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else {
+            node.right = put(node.right, key, value);
+        }
+        node.size = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    public K minKey() {
+        return min(root).key;
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return min(node.left);
+    }
+
+    public void removeMin() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        root = removeMin(root);
+    }
+
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            return node.right;
+        }
+        node.left = removeMin(node.left);
+        node.size = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    public void remove(K key) {
+        checkKeyNotNull(key);
+        root = remove(root, key);
+    }
+
+    private Node remove(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = remove(node.left, key);
+        } else if (cmp > 0) {
+            node.right = remove(node.right, key);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+            Node temp = node;
+            node = min(node.right);
+            node.right = removeMin(temp.right);
+            node.left = temp.left;
+        }
+        node.size = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    public boolean isBalanced() {
+        return balanced(root);
+    }
+
+   private boolean balanced(Node node) {
+        return (node == null) ||
+                balanced(node.left) &&
+                        balanced(node.right) &&
+                        Math.abs(height(node.left) - height(node.right)) <= 1;
+    }
+
+    private int height(Node node) {
+        return node == null ? 0 : 1 + Math.max(height(node.left), height(node.right));
+    }
+
+    @Override
+    public String toString() {
+        return toString(root);
+    }
+
+    private String toString(Node node) {
+        if (node == null) {
+            return "";
+        }
+        return toString(node.left) + " " + node.value + " " +
+                toString(node.right);
+    }
+    public void display() {
+        Stack<Node> globalStack = new Stack<>();
+        globalStack.push(root);
+        int nBlanks = 64;
+
+        boolean isRowEmpty = false;
+        System.out.println("................................................................");
+
+        while (!isRowEmpty) {
+            Stack<Node> localStack = new Stack<>();
+
+            isRowEmpty = true;
+            for (int i = 0; i < nBlanks; i++) {
+                System.out.print(" ");
+            }
+
+            while (!globalStack.isEmpty()) {
+                Node tempNode = globalStack.pop();
+                if (tempNode != null) {
+                    System.out.print(tempNode.value);
+                    localStack.push(tempNode.left);
+                    localStack.push(tempNode.right);
+                    if (tempNode.left != null || tempNode.right != null) {
+                        isRowEmpty = false;
+                    }
+                } else {
+                    System.out.print("--");
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+                for (int j = 0; j < nBlanks * 2 - 2; j++) {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+
+            while (!localStack.isEmpty()) {
+                globalStack.push(localStack.pop());
+            }
+
+            nBlanks /= 2;
+        }
+        System.out.println("................................................................");
+    }
+
+
+}
